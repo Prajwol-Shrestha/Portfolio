28fee87ff7c06c1e1ff26ac579f0a63e223eda76
Prajwol-Shrestha <prajwolshrestha08@gmail.com>
2023-06-10 18:53:47 +0545

    style: letter jump on hover animation

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -12,25 +12,45 @@ export default function Landing() {
 
   return (
     <section className="hero-section">
-      <div className="scale-up-center intro">
-        <h1 className="heading"> Prajwol Shrestha </h1>
+      <div className="intro">
+        <h1 class="heading">
+          <span class="letter">P</span>
+          <span class="letter">r</span>
+          <span class="letter">a</span>
+          <span class="letter">j</span>
+          <span class="letter">w</span>
+          <span class="letter">o</span>
+          <span class="letter">l</span>
+          <span class="letter">&nbsp;</span>
+          <span class="letter">&nbsp;</span>
+          <span class="letter">S</span>
+          <span class="letter">h</span>
+          <span class="letter">r</span>
+          <span class="letter">e</span>
+          <span class="letter">s</span>
+          <span class="letter">t</span>
+          <span class="letter">h</span>
+          <span class="letter">a</span>
+        </h1>
+
         <div className="title flex">
           I am a
           <Typewriter
-          I am a
+            I am a
             options={{
-              strings: ["Frontend Developer.", "Python Developer.", "Problem Solver."],
+              strings: ["Frontend Developer.", "React Developer.", "Problem Solver."],
               autoStart: true,
               loop: true,
+              pause: 50
             }}
           />{" "}
         </div>
-          <div className="btn-container">
-            <button className="btn" onClick={preview} > <AiOutlineCloudDownload /> Resume </button>
-            <Link to="/contact">
-              <button className="btn"> Contact </button>
-            </Link>
-          </div>
+        <div className="btn-container">
+          <button className="btn" onClick={preview} > <AiOutlineCloudDownload /> Resume </button>
+          <Link to="/contact">
+            <button className="btn"> Contact </button>
+          </Link>
+        </div>
       </div>
     </section>
   );
