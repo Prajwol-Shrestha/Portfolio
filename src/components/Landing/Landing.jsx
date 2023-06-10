c41aa80a484d68d8b7b7c93314d5e3f9729bedb4
Prajwol-Shrestha <prajwolshrestha08@gmail.com>
2023-06-10 19:19:13 +0545

    add: delay showing intro section + fav icon added

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Typewriter from "typewriter-effect";
 import { AiOutlineCloudDownload } from 'react-icons/ai'
 import { Link } from 'react-router-dom'
 import resumePdf from '../../assets/Resume.pdf'
 
 export default function Landing() {
+  const [show, setShow] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShow(!show)
+    }, [500])
+  }, [])
 
   function preview() {
     window.open(`${resumePdf}`, "_blank");
@@ -12,46 +19,48 @@ export default function Landing() {
 
   return (
     <section className="hero-section">
-      <div className="intro">
-        <h1 class="heading">
-          <span class="letter">P</span>
-          <span class="letter">r</span>
-          <span class="letter">a</span>
-          <span class="letter">j</span>
-          <span class="letter">w</span>
-          <span class="letter">o</span>
-          <span class="letter">l</span>
-          <span class="letter">&nbsp;</span>
-          <span class="letter">&nbsp;</span>
-          <span class="letter">S</span>
-          <span class="letter">h</span>
-          <span class="letter">r</span>
-          <span class="letter">e</span>
-          <span class="letter">s</span>
-          <span class="letter">t</span>
-          <span class="letter">h</span>
-          <span class="letter">a</span>
-        </h1>
+      { show && 
+        <div className="intro">
+          <h1 className="heading">
+            <span className="letter">P</span>
+            <span className="letter">r</span>
+            <span className="letter">a</span>
+            <span className="letter">j</span>
+            <span className="letter">w</span>
+            <span className="letter">o</span>
+            <span className="letter">l</span>
+            <span className="letter">&nbsp;</span>
+            <span className="letter">&nbsp;</span>
+            <span className="letter">S</span>
+            <span className="letter">h</span>
+            <span className="letter">r</span>
+            <span className="letter">e</span>
+            <span className="letter">s</span>
+            <span className="letter">t</span>
+            <span className="letter">h</span>
+            <span className="letter">a</span>
+          </h1>
 
-        <div className="title flex">
-          I am a
-          <Typewriter
+          <div className="title flex">
             I am a
-            options={{
-              strings: ["Frontend Developer.", "React Developer.", "Problem Solver."],
-              autoStart: true,
-              loop: true,
-              pause: 50
-            }}
-          />{" "}
+            <Typewriter
+              I am a
+              options={{
+                strings: ["React Developer.", "Problem Solver."],
+                autoStart: true,
+                loop: true,
+                cursor: '_'
+              }}
+            />{" "}
+          </div>
+          <div className="btn-container">
+            <button className="btn" onClick={preview} > <AiOutlineCloudDownload /> Resume </button>
+            <Link to="/contact">
+              <button className="btn"> Contact </button>
+            </Link>
+          </div>
         </div>
-        <div className="btn-container">
-          <button className="btn" onClick={preview} > <AiOutlineCloudDownload /> Resume </button>
-          <Link to="/contact">
-            <button className="btn"> Contact </button>
-          </Link>
-        </div>
-      </div>
+      }
     </section>
   );
 }
